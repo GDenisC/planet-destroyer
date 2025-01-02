53822ca7737e305b927b52f5134c2bcff5fcfd7a
DenisC <denis0ches00@gmail.com>
2025-01-02 23:42:21 +0700

    minify

--- esbuild.config.js
+++ esbuild.config.js
@@ -7,7 +7,7 @@ const config = {
     entryPoints: ['src/index.ts'],
     outfile: 'public/bundle.js',
     bundle: true,
-    minify: false,//IS_PRODUCTION,
+    minify: IS_PRODUCTION,
     sourcemap: IS_PRODUCTION ? 'external' : 'linked', // save sourcemap on production to check errors
     target: 'es2020',
     format: 'iife',
